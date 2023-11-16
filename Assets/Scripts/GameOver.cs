12a93e55f1d39d3b5afb6fd867df9e2b7b80dd08
ArtemGribanov <artemsyperstar1@gmail.com>
2023-11-16 13:48:37 +0300

    Add game over screen

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameOver : MonoBehaviour
+{
+    public TextMeshProUGUI roundsText;
+
+    private void OnEnable()
+    {
+        roundsText.text = PlayerStats.Rounds.ToString();
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to MENU");
+    }
+}
